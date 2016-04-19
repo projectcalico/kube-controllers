fd76b31940849b17e38958cd31eb1a38cdea9fb9
Casey D <casey.davenport@metaswitch.com>
2016-04-19 10:11:49 -0700

    Freeze libcalico at v0.14.0

--- build.sh
+++ build.sh
@@ -7,8 +7,8 @@ apk -U add --virtual temp python-dev py-pip alpine-sdk python py-setuptools
 
 # Install python dependencies
 pip install --upgrade pip
-pip install -r https://raw.githubusercontent.com/projectcalico/libcalico/master/build-requirements.txt
-pip install git+https://github.com/projectcalico/libcalico.git
+pip install -r https://raw.githubusercontent.com/projectcalico/libcalico/v0.14.0/build-requirements.txt
+pip install git+https://github.com/projectcalico/libcalico.git@v0.14.0
 pip install simplejson 
 
 # Produce a binary - outputs to /dist/policy_agent
