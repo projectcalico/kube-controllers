6424b55d35094713fee0568f99bced0025a82860
Steffen Hanikel <steffen.hanikel@flixbus.com>
2018-02-27 17:09:42 +0100

    Add optional leaderelection support

--- main.go
+++ main.go
@@ -33,6 +33,14 @@ import (
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"github.com/projectcalico/kube-controllers/pkg/controllers/node"
+	"github.com/projectcalico/kube-controllers/pkg/tools/leaderelection"
+	rl "github.com/projectcalico/kube-controllers/pkg/tools/leaderelection/resourcelock"
+	"k8s.io/client-go/tools/record"
+	"github.com/golang/glog"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/pkg/api/v1"
+	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"time"
 )
 
 // VERSION is filled out during the build process (using git describe output)
@@ -81,6 +89,25 @@ func main() {
 	stop := make(chan struct{})
 	defer close(stop)
 
+	if config.EnableLeaderelection {
+		le := leaderElection(config, k8sClientset, func (stop2 <-chan struct{}) {
+			runControllers(config, k8sClientset, calicoClient, stop, extensionsClient)
+		},func () {
+			// if we lose our leadership shutdown all controllers
+			close(stop)
+		})
+
+		// waits forever
+		le.Run()
+	} else {
+		runControllers(config, k8sClientset, calicoClient, stop, extensionsClient)
+
+		// Wait forever.
+		select {}
+	}
+}
+
+func runControllers(config *config.Config, k8sClientset *kubernetes.Clientset, calicoClient *client.Client, stop chan struct{}, extensionsClient *rest.RESTClient) {
 	for _, controllerType := range strings.Split(config.EnabledControllers, ",") {
 		switch controllerType {
 		case "workloadendpoint":
@@ -99,9 +126,51 @@ func main() {
 			log.Fatalf("Invalid controller '%s' provided. Valid options are workloadendpoint, profile, policy", controllerType)
 		}
 	}
+}
 
-	// Wait forever.
-	select {}
+func leaderElection(config *config.Config, k8sClientset *kubernetes.Clientset, onStartedLeading func (stop <-chan struct{}), onStoppedLeading func ()) *leaderelection.LeaderElector {
+	leaseDuration, err := time.ParseDuration(config.LeaderelectionLeaseDuration)
+	if err != nil {
+		log.WithError(err).Fatal("Can't parse LeaderElection-LeaseDuration.")
+	}
+
+	hostname := config.LeaderelectionIdentity
+	if hostname == "" {
+		hostname, err = os.Hostname()
+		if err != nil {
+			log.WithError(err).Fatal("Can't determine hostname. Unsafe to continue with leader election.")
+		}
+	}
+
+	rlc := rl.ResourceLockConfig{Identity:hostname, EventRecorder:eventRecorder(k8sClientset, hostname)}
+	resourceLock, err := rl.New(rl.ConfigMapsResourceLock,config.LeaderelectionNamespace, config.LeaderelectionName, k8sClientset.CoreV1(), rlc)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to configure resource lock for leader election")
+	}
+	lec := leaderelection.LeaderElectionConfig{
+		Lock:          resourceLock,
+		LeaseDuration: leaseDuration,
+		RenewDeadline: leaseDuration/2,
+		RetryPeriod:   leaseDuration/5,
+		Callbacks: leaderelection.LeaderCallbacks{OnStartedLeading: onStartedLeading, OnStoppedLeading: onStoppedLeading},
+	}
+	le, err := leaderelection.NewLeaderElector(lec)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to confgiure leader election")
+	}
+	return le
+}
+
+func eventRecorder(kubeClient *kubernetes.Clientset, hostname string) record.EventRecorder {
+	eventBroadcaster := record.NewBroadcaster()
+	eventBroadcaster.StartLogging(glog.Infof)
+	eventBroadcaster.StartRecordingToSink(
+		&typedcorev1.EventSinkImpl{
+			Interface: kubeClient.CoreV1().Events("")})
+	recorder := eventBroadcaster.NewRecorder(
+		scheme.Scheme,
+		v1.EventSource{Component: "calico-kube-controllers", Host: hostname})
+	return recorder
 }
 
 // getClients builds and returns Kubernetes, Calico and Extensions clients.
