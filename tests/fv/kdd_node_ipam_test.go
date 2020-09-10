164610207e3034ccb5a67793bd4cf817b7188f5f
Casey Davenport <davenport.cas@gmail.com>
2020-09-09 17:03:06 -0700

    Fix tests

--- tests/fv/kdd_node_ipam_test.go
+++ tests/fv/kdd_node_ipam_test.go
@@ -346,16 +346,28 @@ var _ = Describe("kube-controllers FV tests (KDD mode)", func() {
 				return nil
 			}, time.Second*10, 500*time.Millisecond).Should(BeNil())
 
-			// Assert all IPAM data is removed now.
-			kvps, err := bc.List(context.Background(), model.BlockListOptions{}, "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(len(kvps.KVPairs)).To(Equal(0))
-			kvps, err = bc.List(context.Background(), model.BlockAffinityListOptions{}, "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(len(kvps.KVPairs)).To(Equal(0))
-			kvps, err = bc.List(context.Background(), model.IPAMHandleListOptions{}, "")
-			Expect(err).NotTo(HaveOccurred())
-			Expect(len(kvps.KVPairs)).To(Equal(0))
+			Eventually(func() error {
+				// Assert all IPAM data is removed now.
+				kvps, err := bc.List(context.Background(), model.BlockListOptions{}, "")
+				if err != nil {
+					return err
+				} else if len(kvps.KVPairs) != 0 {
+					return fmt.Errorf("Expected no blocks but there are some")
+				}
+				kvps, err = bc.List(context.Background(), model.BlockAffinityListOptions{}, "")
+				if err != nil {
+					return err
+				} else if len(kvps.KVPairs) != 0 {
+					return fmt.Errorf("Expected no affinities but there are some")
+				}
+				kvps, err = bc.List(context.Background(), model.IPAMHandleListOptions{}, "")
+				if err != nil {
+					return err
+				} else if len(kvps.KVPairs) != 0 {
+					return fmt.Errorf("Expected no handles but there are some")
+				}
+				return nil
+			}, time.Second*10, 500*time.Millisecond).Should(BeNil())
 		})
 	})
 })
@@ -366,7 +378,7 @@ func assertNumBlocks(bc backend.Client, num int) error {
 		return fmt.Errorf("error querying blocks: %s", err)
 	}
 	if len(blocks.KVPairs) != num {
-		return fmt.Errorf("Expected 0 blocks, found %d. Blocks: %#v", len(blocks.KVPairs), blocks)
+		return fmt.Errorf("Expected %d blocks, found %d. Blocks: %#v", num, len(blocks.KVPairs), blocks)
 	}
 	return nil
 }
